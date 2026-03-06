c8289afd35974a2dc000192d04340b2f396cfd81
sebastim <sebastian.mais@ut.ee>
2026-03-06 16:51:51 +0200

    create project boilerplate using SpringBoot and Vuejs

---
+++ backend/src/main/java/com/example/cgi_resto/CgiRestoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgi_resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiRestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiRestoApplication.class, args);
+	}
+
+}
