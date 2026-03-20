f24599e7ba5d2ae6ead85b529af3dc9bc094532c
sebastim <sebastian.mais@ut.ee>
2026-03-20 18:22:31 +0200

    add tailwindcss for easier customization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <ul>
-  <li v-for="laud in lauad" :key="laud.lauaNr">
-    Laud {{ laud.lauaNr }}: {{ laud.kohtadeArv }} istet
-  </li>
-</ul>
+  <div class="parent">
+    <div class="div1 ">
+       Broneeri laud
+    </div>
+    <div class="div2">
+       <ul>
+          <li v-for="laud in lauad" :key="laud.lauaNr">
+            Laud {{ laud.lauaNr }}: {{ laud.kohtadeArv }} istet
+          </li>
+        </ul>
+    </div>
+</div>
+
 </template>
 
 <script setup>
+  import "tailwindcss"
   import { ref, onMounted } from 'vue';
 
   const lauad = ref([]);
@@ -22,4 +31,11 @@
   })
 </script>
 
-<style scoped></style>
+<style scoped>
+.parent {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(1, 1fr);
+    gap: 8px;
+}
+</style>
