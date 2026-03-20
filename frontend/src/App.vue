4a9fd4a6013b8370a36f00572ad2befc375b3e74
sebastim <sebastian.mais@ut.ee>
2026-03-20 19:59:27 +0200

    add search for an available reservation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="parent">
-    <div class="div1 ">
-       Broneeri laud
+    <div class="div1"> Broneeri laud
+      <div>Mitu inimest? (max 10)
+        <InputNumber v-model="inimesteArv" showButtons buttonLayout="vertical" inputId="minmax" :min="1" :max="10" style="width: 3rem;" />
+      </div>
+       <div>
+          Valige kuupäev ja kellaaeg:
+          <DatePicker v-model="valitudKuupäev" showIcon iconDisplay="input" />
+          <DatePicker v-model="valitudKell" showIcon iconDisplay="input" timeOnly/>
+       </div>
+       <div>Soovitatud lauad: {{ vabadLauad }}</div>
     </div>
+    
     <div class="div2">
        <ul>
           <li v-for="laud in lauad" :key="laud.lauaNr">
-            Laud {{ laud.lauaNr }}: {{ laud.kohtadeArv }} istet
+            Laud {{ laud.lauaNr }}: {{ laud.kohtadeArv }} istet, {{ laud.reserveeringud.length }} reserveeringut
           </li>
         </ul>
     </div>
@@ -15,10 +24,17 @@
 </template>
 
 <script setup>
-  import "tailwindcss"
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, computed } from 'vue';
+  import DatePicker from 'primevue/datepicker'
+  import InputNumber from 'primevue/inputnumber';
 
+  const date = ref(null)
   const lauad = ref([]);
+  const inimesteArv = ref(1);
+  const valitudKuupäev = ref(null);
+  const valitudKell = ref(null);
+
+  const minDate = new Date(); //tänane kuupäev
   onMounted(() => {
     fetch("/api/reserveeringud")
     .then((response) => response.json())
@@ -29,6 +45,29 @@
       console.error("Error fetching data:", error);
     });
   })
+
+ const vabadLauad = computed(() => {
+  if (!valitudKuupäev.value || !valitudKell.value) {
+    return "Palun valige kuupäev ja kellaaeg.";
+  }
+  const soovitudAeg = new Date(valitudKuupäev.value);
+  soovitudAeg.setHours(valitudKell.value.getHours());
+  soovitudAeg.setMinutes(valitudKell.value.getMinutes());
+
+  const vabad = lauad.value.filter(laud => {
+    if (laud.kohtadeArv < inimesteArv.value) {
+      return false; // ei sobi, kui laual puudub piisav arv kohti
+    }
+    return laud.reserveeringud.every(reserveering => {
+      const reserveeringAeg = new Date(reserveering);
+      return reserveeringAeg.getTime() < soovitudAeg.getTime() - 2 * 60 * 60 * 1000 ||
+             reserveeringAeg.getTime() > soovitudAeg.getTime() + 2 * 60 * 60 * 1000;
+    });
+  });
+
+  return vabad.length > 0 ? vabad.map(laud => laud.lauaNr).join(", ") : "Puuduvad vabad lauad.";
+});
+
 </script>
 
 <style scoped>
